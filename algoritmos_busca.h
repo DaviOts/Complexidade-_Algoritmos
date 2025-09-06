3590d5211fc680fc0595d6d32d6a8e877ea41ed2
DaviOts <daviotaviano1@gmail.com>
2025-09-06 15:04:50 -0300

    criação da minha interface para algoritmos

---
+++ algoritmos_busca.h
@@ -0,0 +1,17 @@
+#pragma once // uma forma mais moderna do ifndef/define/endif e evita erros definição múltipla
+
+#include <vector>
+#include <string>
+#include <memory>
+
+// Interface base para algoritmos de busca
+class AlgoritmoBusca {
+public:
+    virtual ~AlgoritmoBusca() = default; // destrutor virtual padrão, ele é chamado quando um objeto é destruído
+    virtual std::string nome() const = 0; // retorna o nome do algoritmo de busca
+    virtual int buscar(const std::vector<int>& dados, int chave) const = 0; // executa a busca e retorna o índice ou -1 se não for encontrado
+};
+
+// Fábricas para criar instâncias concretas de algoritmos
+std::unique_ptr<AlgoritmoBusca> criar_busca_sequencial();
+std::unique_ptr<AlgoritmoBusca> criar_busca_binaria();
