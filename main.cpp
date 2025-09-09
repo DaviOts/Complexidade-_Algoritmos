36591708a274c7b9610bbe7346cdcb85369e51bf
DaviOts <78978192+DaviOts@users.noreply.github.com>
2025-09-09 18:04:47 -0300

    enxugando o main

--- main.cpp
+++ main.cpp
@@ -8,25 +8,22 @@ int main() {
     std::vector<int> dados = {1, 3, 5, 7, 9, 11, 13, 15, 17, 19};
     int chave = 7;
 
-    // Criamos as instâncias dos algoritmos de busca usando as fábricas
-    std::unique_ptr<AlgoritmoBusca> busca_sequencial = criar_busca_sequencial();
-    std::unique_ptr<AlgoritmoBusca> busca_binaria = criar_busca_binaria();
+    
 
     // Executa a busca sequencial
+    // Esse auto substitui o "std::unique_ptr", ele ja pede pro compilador preencher esse tipo, sem eu precisar explicita-lo
+    auto busca_sequencial = criar_busca_sequencial();
+    std::cout << "Algoritmo: " << busca_sequencial->nome() << "\n";
+    std::cout << "Chave buscada: " << chave << "\n";
     int indice_sequencial = busca_sequencial->buscar(dados, chave);
-    if (indice_sequencial != -1) {
-        std::cout << busca_sequencial->nome() << ": Chave " << chave << " foi encontrada no índice: " << indice_sequencial << std::endl;
-    } else {
-        std::cout << busca_sequencial->nome() << ": Chave " << chave << " Não foi encontrada" << std::endl;
-    }
+    std::cout << "Resultado: " << (indice_sequencial != -1 ? "encontrado no índice " + std::to_string(indice_sequencial) : "não encontrado") << "\n";
 
     // Executa a busca binária
+    auto busca_binaria = criar_busca_binaria();
+    std::cout << "Algoritmo: " << busca_binaria->nome() << "\n";
+    std::cout << "Chave buscada: " << chave << "\n";
     int indice_binaria = busca_binaria->buscar(dados, chave);
-    if (indice_binaria != -1) {
-        std::cout << busca_binaria->nome() << ": Chave " << chave << " Foi encontrada no índice: " << indice_binaria << std::endl;
-    } else {
-        std::cout << busca_binaria->nome() << ": Chave " << chave << " Não foi encontrada" << std::endl;
-    }
+    std::cout << "Resultado: " << (indice_binaria != -1 ? "encontrado no índice " + std::to_string(indice_binaria) : "não encontrado") << "\n";
 
     return 0;
 }
